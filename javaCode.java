2cc0bb5966e356c381ff5dc210aaad2580bf6ea7
Aniket1703 <you@example.com>
2025-06-07 18:54:42 +0530

    Second Commit from intellij

---
+++ javaCode.java
@@ -0,0 +1,9 @@
+public class javaCode {
+
+    public static void main(String[] args){
+
+    }
+    static void firstFunction(){
+        System.out.println(" second commit from intellij");
+    }
+}
